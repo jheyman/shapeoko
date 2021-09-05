a0f851fc5fc88737e48bf94cf9c2da7487a4681a
jheyman <bidsomail@gmail.com>
2021-09-05 10:46:27 +0200

    misc updates (one version fix for keyboard reader build)

--- Arduino_keypad/KeyboardSerial/KeyboardSerial.ino
+++ Arduino_keypad/KeyboardSerial/KeyboardSerial.ino
@@ -1,8 +1,6 @@
 #include "Keyboard.h"
 
-// buttons in that order:
-// left, right, up, down, zup, zdown, jogstepsmallest, jogstepsmall, jogsteplarge, jogstep fast
-
+//#define SERIAL
 #define BUTTON_XLEFT 16
 #define BUTTON_XRIGHT 14
 #define BUTTON_YUP 15
@@ -13,6 +11,10 @@
 #define BUTTON_JOG2 8
 #define BUTTON_JOG3 7 
 #define BUTTON_JOG4 6
+#define LED_JOG1 5
+#define LED_JOG2 4
+#define LED_JOG3 2
+#define LED_JOG4 3
 const int buttonPin[] = {BUTTON_XLEFT, BUTTON_XRIGHT, BUTTON_YUP, BUTTON_YDOWN, BUTTON_ZUP, BUTTON_ZDOWN, BUTTON_JOG1, BUTTON_JOG2, BUTTON_JOG3, BUTTON_JOG4};
 
 int pinCount = 10;
@@ -22,7 +24,7 @@ long lastDebounceTime[] = {0,0,0,0,0,0,0,0,0,0};
 long debounceDelay = 10;
 
 // LED left to right
-const int ledPin[] = {5, 4, 2, 3};
+const int ledPin[] = {LED_JOG1, LED_JOG2, LED_JOG3, LED_JOG4};
 int ledpinCount = 4;
 
 void setup() {
@@ -37,51 +39,107 @@ void setup() {
     digitalWrite(ledPin[ledpin], LOW);
   }
 
+#ifdef SERIAL
   Serial.begin(9600);
-  //Keyboard.begin();
+#endif
+
+  Keyboard.begin();
+}
+
+void logSerial(char* msg)
+{
+#ifdef KEYBOARD
+  Serial.println(msg);
+#endif
 }
 
 int pressKey(int currentButton) {
-    if (currentButton == 0) {
-      //Keyboard.press(ctrlKey);
-      Keyboard.press('a');
-      delay(100);
-    }
-    else if (currentButton  == 1) {
-      //Keyboard.press(ctrlKey);
-      Keyboard.press('b');
-      delay(100);
-    }
-    else if (currentButton  == 2) {
-      //Keyboard.press(ctrlKey);
-      Keyboard.press('c');
-      delay(100);
-    }
-    else if (currentButton  == 3) {
-      //Keyboard.press(ctrlKey);
-      Keyboard.press('d');
-      delay(100);
-    }
+
+  switch(currentButton) {
+    case BUTTON_XLEFT:
+     Keyboard.press(KEY_LEFT_ARROW);
+     delay(5);
+     Keyboard.releaseAll();
+     logSerial("keypressed: XLEFT");
+      break;
+    case BUTTON_XRIGHT:
+     Keyboard.press(KEY_RIGHT_ARROW);
+     delay(5);
+     Keyboard.releaseAll(); 
+     logSerial("keypressed: XRIGHT");
+     break;
+    case BUTTON_YUP:
+     Keyboard.press(KEY_UP_ARROW);
+     delay(5);
+     Keyboard.releaseAll();  
+     logSerial("keypressed: YUP");
+     break;
+    case BUTTON_YDOWN:
+     Keyboard.press(KEY_DOWN_ARROW);
+     delay(5);
+     Keyboard.releaseAll();  
+     logSerial("keypressed: YDOWN"); 
+     break;
+    case BUTTON_ZUP:
+     Keyboard.press(KEY_PAGE_UP);
+     delay(5);
+     Keyboard.releaseAll(); 
+     logSerial("keypressed: ZUP");  
+     break;
+    case BUTTON_ZDOWN:
+     Keyboard.press(KEY_PAGE_UP);
+     delay(5);
+     Keyboard.releaseAll(); 
+     logSerial("keypressed: ZDOWN"); 
+     break;
+    case BUTTON_JOG1:
+     Keyboard.press('1');
+     delay(5);
+     Keyboard.releaseAll(); 
+     logSerial("keypressed: JOG1");    
+     digitalWrite(LED_JOG1, HIGH);
+     digitalWrite(LED_JOG2, LOW);
+     digitalWrite(LED_JOG3, LOW);
+     digitalWrite(LED_JOG4, LOW);
+     break;
+    case BUTTON_JOG2:
+     Keyboard.press('2');
+     delay(5);
+     Keyboard.releaseAll(); 
+     logSerial("keypressed: JOG2");    
+     digitalWrite(LED_JOG1, LOW);
+     digitalWrite(LED_JOG2, HIGH);
+     digitalWrite(LED_JOG3, LOW);
+     digitalWrite(LED_JOG4, LOW);
+     break;  
+    case BUTTON_JOG3:
+     Keyboard.press('3');
+     delay(5);
+     Keyboard.releaseAll(); 
+     logSerial("keypressed: JOG3");    
+     digitalWrite(LED_JOG1, LOW);
+     digitalWrite(LED_JOG2, LOW);
+     digitalWrite(LED_JOG3, HIGH);
+     digitalWrite(LED_JOG4, LOW);        
+     break;  
+    case BUTTON_JOG4:
+     Keyboard.press('4');
+     delay(5);
+     Keyboard.releaseAll(); 
+     logSerial("keypressed: JOG4");    
+     digitalWrite(LED_JOG1, LOW);
+     digitalWrite(LED_JOG2, LOW);
+     digitalWrite(LED_JOG3, LOW);
+     digitalWrite(LED_JOG4, HIGH);      
+     break;                             
+    default:
+     break;
+  }
 }
 
 int releaseKey(int currentButton) {
-    if (currentButton == 0) {
-      //Keyboard.press(ctrlKey);
-      Keyboard.release('a');
-    }
-    else if (currentButton  == 1) {
-      //Keyboard.press(ctrlKey);
-      Keyboard.release('b');
-    }
-    else if (currentButton  == 2) {
-      //Keyboard.press(ctrlKey);
-      Keyboard.release('c');
-    }
-    else if (currentButton  == 3) {
-      //Keyboard.press(ctrlKey);
-      Keyboard.release('d');
-    }
 }
+
 void loop() {
   for (int pin = 0; pin < pinCount; pin++) {
     
@@ -91,40 +149,11 @@ void loop() {
       if ((millis() - lastDebounceTime[pin]) > debounceDelay) {
 
         if (buttonState[pin] == LOW){
-          //pressKey(pin);
-          Serial.print("keypressed:");
-          Serial.println(pin);
-
-          switch(pin) {
-            case 3:
-              digitalWrite(ledPin[3], HIGH);
-              delay(200);
-              digitalWrite(ledPin[3], LOW);
-              break;
-            case 2:
-              digitalWrite(ledPin[2], HIGH);
-              delay(200);
-              digitalWrite(ledPin[2], LOW);            
-             break;
-            case 1:
-              digitalWrite(ledPin[1], HIGH);
-              delay(200);
-              digitalWrite(ledPin[1], LOW);
-             break;
-            case 0:
-              digitalWrite(ledPin[0], HIGH);
-              delay(200);
-              digitalWrite(ledPin[0], LOW);
-             break;
-            default:
-             break;
-          }
+          pressKey(buttonPin[pin]);
         } else {
-          //releaseKey(pin);
-          Serial.print("keyreleased:");
-          Serial.println(pin);
+          releaseKey(buttonPin[pin]);
         }
-        
+       
         lastDebounceTime[pin] = millis();
       }
     }
